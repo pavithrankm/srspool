5262024422328316e39ee88ae9f8c6c794098d2c
pavithrankm <103016602+pavithrankm@users.noreply.github.com>
2022-05-31 13:07:48 +0530

    Update BasePage.java

--- src/main/java/SRSproject/SRSproject/BasePage.java
+++ src/main/java/SRSproject/SRSproject/BasePage.java
@@ -35,7 +35,7 @@ public class BasePage
 
 	//PCS
 	//	public static String configpath ="src/main/java/config_staging/configuration_staging_pcs.properties";
-	public static String configpath="src/main/java/config_staging2/configuration_staging2_pcs.properties";
+	//public static String configpath="src/main/java/config_staging2/configuration_staging2_pcs.properties";
 	//	public static String configpath ="src/main/java/config_prod/config_prod_pcs.properties";
 	//	public static String configpath ="src/main/java/config/configuration_pcs.properties"; //QA
 	
@@ -51,7 +51,7 @@ public class BasePage
 	//	public static String configpath= "src/main/java/config_staging2/configuration_staging2_glb.properties";
 	
 	//Aqua-gon
-	//public static String configpath="src/main/java/config_staging2/configuration_staging2_Aquagon.properties";
+	public static String configpath="src/main/java/config_staging2/configuration_staging2_Aquagon.properties";
 	//public static String configpath="src/main/java/config_prod/config_prod_Aquagon.properties";
 		
 	//PEP	
