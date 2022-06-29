09b9c36f86e8f37456328227c720a12372ddf52a
pavithrankm <103016602+pavithrankm@users.noreply.github.com>
2022-06-29 10:54:32 +0530

    Update UnAuthorizedAccessToConely.java

--- src/test/java/SRSproject/LoginTests/UnAuthorizedAccessToConely.java
+++ src/test/java/SRSproject/LoginTests/UnAuthorizedAccessToConely.java
@@ -26,7 +26,6 @@ public class UnAuthorizedAccessToConely extends BaseTest {
 		Hpg = new HomePage_Global(driver);
 
 		if (driver.getCurrentUrl().equals(prop.get("ConelyLoginPagePoolURL"))) {
-
 			Hpg.ValidLoginAgon();
 			LoginPage Lp = new LoginPage(driver);
 			Thread.sleep(3000);
